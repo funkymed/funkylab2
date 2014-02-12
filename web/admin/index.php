0869f5844b366316f240bc8808e77ba7e2b2eac8
Cyril Pereira <cyril.pereira@gmail.com>
2014-02-12 13:22:54 +0100

    remove patch finally

--- web/admin/index.php
+++ web/admin/index.php
@@ -1,11 +1,10 @@
 <?php
 	header("Content-type: text/html");
-	/*header("Content-Disposition: filename=index.php\r\n\r\n");
+	header("Content-Disposition: filename=index.php\r\n\r\n");
 	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");             
 	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
 	header("Cache-Control: no-cache, must-revalidate"); 
 	header("Pragma: no-cache");  
-	*/
 	session_start();
 
 	/**************************************************
